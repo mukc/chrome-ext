539ae5848f51f43de14fba9cda2708cb9007cee7
Uday Mallineni <udaymallineni@gmail.com>
2022-09-05 22:46:14 -0500

    Update content.js
    cleanup the code and safety checks

--- content.js
+++ content.js
@@ -1,77 +1,93 @@
 /**
  * initiate the process
  */
-startActions()
+startActions();
 
-function startActions () {
-    setTimeout(browserActions, 1250)
+function startActions() {
+    setTimeout(browserActions, 1250);
 }
 
 /**
  * open the CNBC website and navigate to NASDAQ tab in Market Movers section
  */
-function browserActions () {
-    var buttons = document.getElementsByTagName('button')
+function browserActions() {
 
-    let clickButton = ''
+    var buttons = document.getElementsByTagName('button');
+
+    let clickButton = '';
 
     for (i = 0; i < buttons.length; i++) {
+
         if (buttons[i].textContent.includes('NASDAQ')) {
-            clickButton = buttons[i]
+            clickButton = buttons[i];
         }
     }
 
     if (clickButton) {
-        clickButton.click()
+        clickButton.click();
     }
-    fetchSecondHighestGainedStock()
+
+    fetchSecondHighestGainedStock();
 }
 
 /**
  * wait until the page content is fully loaded and then
  * Fetch Top second gained stock of the day and call new tab open method
  */
-function fetchSecondHighestGainedStock () {
+function fetchSecondHighestGainedStock() {
+
     setTimeout(() => {
-        const existingTables = document.getElementsByClassName('MarketTop-topTable')
+
+        const existingTables = document.getElementsByClassName('MarketTop-topTable');
 
         if (!existingTables) {
-            return false
+            return false;
         }
 
-        let topGainers = existingTables[0]
+        let topGainers = existingTables[0];
+
         if (!topGainers) {
-            return false
+            return false;
         }
 
-        let secondHighestGainer = topGainers.rows[1]
+        let secondHighestGainer = topGainers.rows[1];
+        
+        let stockName = 'N/A';
+        let percentGain = 'N/A';
+        
+        if (secondHighestGainer) {
+            if (secondHighestGainer.cells[1]) {
+                stockName = secondHighestGainer.cells[1].innerText;
+            }
+            
+            if (secondHighestGainer.cells[3]) {
+                percentGain = secondHighestGainer.cells[3].innerText ? secondHighestGainer.cells[3].innerText.slice(0, -1) : 'N/A'; //slice off the % symbol in the end
+            }
+        }
 
-        const stockName = secondHighestGainer.cells[1].innerText
-        const percentGain = secondHighestGainer.cells[3].innerText.slice(0, -1) //slice off the % symbol in the end
-        const timestamp = Date.now() //Includes Time zone
+        const timestamp = Date.now(); //Includes Time zone
 
-        openNewTab(stockName, percentGain, timestamp)
+        openNewTab(stockName, percentGain, timestamp);
     }, 500)
 }
 
 /**
- * Open a new tab for Zoho form
+ * Open a new tab for Zoho form and pass data from CNBC window to Zoho window
  * @param stockName
  * @param dailyGain
  * @param timestamp
  */
-function openNewTab (stockName, percentGain, timestamp) {
+function openNewTab(stockName, percentGain, timestamp) {
 
-    const formData = {name: stockName, gain: percentGain, time: timestamp}
-
-    const formLink = 'https://forms.zohopublic.in/developers/form/TestResponses/formperma/-gq-UT1RjqASnGgBsW-M8MmPm8e3YLhcyFam06v2piE'
-    let formWindow = window.open(formLink, '_blank')
+    const formData = {name: stockName, gain: percentGain, time: timestamp};
+    const zohoFormLink = 'https://forms.zohopublic.in/developers/form/TestResponses/formperma/-gq-UT1RjqASnGgBsW-M8MmPm8e3YLhcyFam06v2piE';
+    let formWindow = window.open(zohoFormLink, '_blank');
 
     /**
      * we need to wait until the Zoho form page is fully loaded to receive the message posted
      */
     setTimeout(function () {
-        formWindow.postMessage(formData, 'https://forms.zohopublic.in')
+        formWindow.postMessage(formData, 'https://forms.zohopublic.in');
     }, 2000)
 }
 
@@ -80,25 +96,29 @@ function openNewTab (stockName, percentGain, timestamp) {
  * only considering Zoho form page as we need it to auto fill the form once it is loaded
  */
 window.onload = function () {
+
     if (window.location.href.includes('zohopublic')) {
+
         window.addEventListener('message', function (event) {
+
             if (window.location.href.includes('zohopublic')) {
 
                 /**
                  * for security purpose,
-                 * Make sure we are only looking for the messages originated from CNBC domain
+                 * make sure we are only looking for the messages originated from CNBC domain
                  */
                 if (event.origin !== 'https://www.cnbc.com') {
-                    return
+                    return;
                 }
 
                 /**
-                 * auto fill the zoho form
+                 * auto fill the zoho form with stock data
                  */
-                document.getElementsByName('SingleLine')[0].value = event.data.name
-                document.getElementsByName('SingleLine1')[0].value = event.data.gain
-                document.getElementsByName('SingleLine2')[0].value = event.data.time
+                document.getElementsByName('SingleLine')[0].value = event.data.name;
+                document.getElementsByName('SingleLine1')[0].value = event.data.gain;
+                document.getElementsByName('SingleLine2')[0].value = event.data.time;
             }
+    
         }, false)
     }
 }
